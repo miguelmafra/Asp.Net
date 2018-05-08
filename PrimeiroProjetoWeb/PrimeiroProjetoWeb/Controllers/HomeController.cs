9a554cdc642fc7011fc10a6e195dc44f7e299f53
miguel lorenzi <miguelzinho159357@hotmail.com>
2018-05-08 20:39:23 -0300

    Pequenas configurações

--- PrimeiroProjetoWeb/PrimeiroProjetoWeb/Controllers/HomeController.cs
+++ PrimeiroProjetoWeb/PrimeiroProjetoWeb/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace PrimeiroProjetoWeb.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Página de descrição da aplicação";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Página de contato";
 
             return View();
         }
