a756f75e18d3af2b84eae5f78507337192d898dd
miguelmafra <miguelml159357@gmail.com>
2018-05-22 21:49:09 -0300

    Adicionando string de conexão

--- PrimeiroProjetoWeb/Aula2205_Entity/Models/DAL/MeuContexto.cs
+++ PrimeiroProjetoWeb/Aula2205_Entity/Models/DAL/MeuContexto.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Data.Entity;
 
 namespace Aula2205_Entity.Models.DAL
 {
-    public class MeuContexto
+    public class MeuContexto : DbContext
     {
+        public MeuContexto() : base("strConn")
+        {
 
+        }
     }
 }
