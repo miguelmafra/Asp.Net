55b32bc0dec21de0b5b527f2765bdad1131ee001
miguelmafra <miguelml159357@gmail.com>
2018-05-24 19:46:00 -0300

    Criando classe usuario e atividade

---
+++ PrimeiroProjetoWeb/Aula2205_Entity/Models/Atividade.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Aula2205_Entity.Models.DAL
+{
+    public class Atividade
+    {
+        [Key]
+        public int AtividadeID { get; set; }
+
+        [Required, StringLength(30)]
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public bool Ativo { get; set; }
+
+        public int CategoriaID { get; set; }
+
+        public virtual Categoria _Categoria { get; set; }
+
+        public int UsuarioID { get; set; }
+
+        public virtual Usuario _Usuario { get; set; }
+
+    }
+
+}
---
+++ PrimeiroProjetoWeb/Aula2205_Entity/Models/Categoria.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Aula2205_Entity.Models
+{
+    public class Categoria
+    {
+
+        public int CategoriaID { get; set; }
+
+        [Required,StringLength(30)]
+        public string Nome { get; set; }
+
+        public bool Ativo { get; set; }
+
+
+    }
+}
