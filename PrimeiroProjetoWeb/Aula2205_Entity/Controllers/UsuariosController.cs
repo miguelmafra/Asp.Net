17ebf22e7d7ed90665807447b065478a805a2429
miguelmafra <miguelml159357@gmail.com>
2018-05-24 21:36:33 -0300

    Criando Action e View 'Details' para Usuarios

--- PrimeiroProjetoWeb/Aula2205_Entity/Controllers/UsuariosController.cs
+++ PrimeiroProjetoWeb/Aula2205_Entity/Controllers/UsuariosController.cs
@@ -3,6 +3,7 @@ using Aula2205_Entity.Models.DAL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -38,6 +39,26 @@ namespace Aula2205_Entity.Controllers
             }
             return View(usuario);
         }
+
+        public ActionResult Details(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            MeuContexto contexto = new MeuContexto();
+
+            Usuario usu = contexto.Usuarios.Find(id);
+
+            if(usu == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(usu);
+        }
+
 
     }
   
